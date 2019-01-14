8f88ebbbfcd469fc8c0f1d417665e7b9054d5aa6
Tairine <taimousinho@gmail.com>
2019-01-14 12:28:12 -0300

    Inclusão de meu-projeto2

--- src/app/meu-primeiro/meu-primeiro.component.ts
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-meu-primeiro',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    selector: 'app-meu-primeiro-component',
+    template: `
+    Meu Primeiro Componente!
+    `,
   })
   export class MeuPrimeiroComponent {
     title = 'app';
